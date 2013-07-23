acca4ebe5be0e97725e155165c3bb8e35436d330
Jason Barnett <jason.w.barnett@gmail.com>
2013-07-23 08:59:59 -0700

    -cleaned things up so it's easier to read, ruby-ized things -added end frame to cinema4d_sg.py

--- etc/cinema4d_sg.py
+++ etc/cinema4d_sg.py
@@ -29,52 +29,58 @@ import os,signal,subprocess,sys
 os.umask(0)
 
 # fetch DrQueue environment
-DRQUEUE_BLOCKSIZE = int(os.getenv("DRQUEUE_BLOCKSIZE"))
-DRQUEUE_COMPID = int(os.getenv("DRQUEUE_COMPID"))
-DRQUEUE_ENDFRAME = int(os.getenv("DRQUEUE_ENDFRAME"))
-DRQUEUE_ETC = os.getenv("DRQUEUE_ETC")
-DRQUEUE_FRAME = int(os.getenv("DRQUEUE_FRAME"))
-DRQUEUE_JOBID = int(os.getenv("DRQUEUE_JOBID"))
-DRQUEUE_JOBNAME = os.getenv("DRQUEUE_JOBNAME")
-DRQUEUE_OS = os.getenv("DRQUEUE_OS")
-DRQUEUE_OWNER = os.getenv("DRQUEUE_OWNER")
-DRQUEUE_PADFRAME = int(os.getenv("DRQUEUE_PADFRAME"))
-DRQUEUE_PADFRAMES = int(os.getenv("DRQUEUE_PADFRAMES"))
+DRQUEUE_BLOCKSIZE  = int(os.getenv("DRQUEUE_BLOCKSIZE"))
+DRQUEUE_COMPID     = int(os.getenv("DRQUEUE_COMPID"))
+DRQUEUE_ENDFRAME   = int(os.getenv("DRQUEUE_ENDFRAME"))
+DRQUEUE_ETC        = os.getenv("DRQUEUE_ETC")
+DRQUEUE_FRAME      = int(os.getenv("DRQUEUE_FRAME"))
+DRQUEUE_JOBID      = int(os.getenv("DRQUEUE_JOBID"))
+DRQUEUE_JOBNAME    = os.getenv("DRQUEUE_JOBNAME")
+DRQUEUE_OS         = os.getenv("DRQUEUE_OS")
+DRQUEUE_OWNER      = os.getenv("DRQUEUE_OWNER")
+DRQUEUE_PADFRAME   = int(os.getenv("DRQUEUE_PADFRAME"))
+DRQUEUE_PADFRAMES  = int(os.getenv("DRQUEUE_PADFRAMES"))
 DRQUEUE_STARTFRAME = int(os.getenv("DRQUEUE_STARTFRAME"))
-DRQUEUE_STEPFRAME = int(os.getenv("DRQUEUE_STEPFRAME"))
+DRQUEUE_STEPFRAME  = int(os.getenv("DRQUEUE_STEPFRAME"))
 
 
 if DRQUEUE_OS == "WINDOWS":
+	THREADS = 0
+
 	# convert to windows path with drive letter
-	SCENE=subprocess.Popen(["cygpath.exe", "-w "+SCENE], stdout=subprocess.PIPE).communicate()[0]
-	RENDERDIR=subprocess.Popen(["cygpath.exe", "-w "+RENDERDIR], stdout=subprocess.PIPE).communicate()[0]
+	SCENE     = subprocess.Popen(["cygpath.exe", "-w " + SCENE], stdout=subprocess.PIPE).communicate()[0]
+	RENDERDIR = subprocess.Popen(["cygpath.exe", "-w " + RENDERDIR], stdout=subprocess.PIPE).communicate()[0]
 	
-	ENGINE_PATH="C:\Program\ Files\MAXON\CINEMA\ 4D\ R10\CINEMA\ 4D.exe"
-	LOGFILE="C:\Program\ Files\MAXON\CINEMA\ 4D\ R10\RENDERLOG.TXT"
+	ENGINE_PATH = "C:\Program\ Files\MAXON\CINEMA\ 4D\ R10\CINEMA\ 4D.exe"
+	LOGFILE     = "C:\Program\ Files\MAXON\CINEMA\ 4D\ R10\RENDERLOG.TXT"
 
 if DRQUEUE_OS == "OSX":
-	ENGINE_PATH="/Applications/MAXON/CINEMA\ 4D\ R10/CINEMA\ 4D.app/Contents/MacOS/CINEMA\ 4D"
-	LOGFILE="/Applications/MAXON/CINEMA\ 4D\ R10/CINEMA\ 4D.app/Contents/MacOS/CINEMA\ 4D/RENDERLOG.TXT"    
+	THREADS     = subprocess.Popen("/usr/sbin/sysctl hw.ncpu | awk '{print $2}'", shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
+
+	ENGINE_PATH = "/Applications/MAXON/CINEMA\ 4D\ R10/CINEMA\ 4D.app/Contents/MacOS/CINEMA\ 4D"
+	LOGFILE     = "/Applications/MAXON/CINEMA\ 4D\ R10/CINEMA\ 4D.app/Contents/MacOS/CINEMA\ 4D/RENDERLOG.TXT"    
 
 if DRQUEUE_OS == "LINUX":
+	THREADS = 0
+
 	# we use wine on linux (this is a hack, but works)
 	# there is a tightvnc server running on display :1
 	# see wine bug #8069
 	# the user running DrQueue slave process needs to have wine and Cinema4D installed
 	
 	# convert to windows path with drive letter
-	SCENE=subprocess.Popen(["winepath", "-w "+SCENE], stdout=subprocess.PIPE).communicate()[0]
-	RENDERDIR=subprocess.Popen(["winepath", "-w "+RENDERDIR], stdout=subprocess.PIPE).communicate()[0]
+	SCENE     = subprocess.Popen(["winepath", "-w " + SCENE], stdout=subprocess.PIPE).communicate()[0]
+	RENDERDIR = subprocess.Popen(["winepath", "-w " + RENDERDIR], stdout=subprocess.PIPE).communicate()[0]
 	
-	WORKDIR="~/.wine/drive_c/Program\ Files/MAXON/CINEMA\ 4D\ R10"
-	ENGINE_PATH="wine CINEMA\ 4D.exe"
-	LOGFILE="~/.wine/drive_c/Program\ Files/MAXON/CINEMA\ 4D\ R10/RENDERLOG.TXT"
+	WORKDIR     = "~/.wine/drive_c/Program\ Files/MAXON/CINEMA\ 4D\ R10"
+	ENGINE_PATH = "wine CINEMA\ 4D.exe"
+	LOGFILE     = "~/.wine/drive_c/Program\ Files/MAXON/CINEMA\ 4D\ R10/RENDERLOG.TXT"
 	
 	# change into workdir, better for wine startup
 	os.chdir(WORKDIR)
 	
 	# set env variable, so wine can access the xserver even though we are rendering headless
-	os.environ["DISPLAY"]=":1"
+	os.environ["DISPLAY"] = ":1"
 
 
 BLOCK = DRQUEUE_FRAME + DRQUEUE_BLOCKSIZE - 1
@@ -86,13 +92,13 @@ if BLOCK > DRQUEUE_ENDFRAME:
 # delete old logfile
 os.remove(LOGFILE)
 
-command = ENGINE_PATH+"-nogui -render "+SCENE+" -oimage "+RENDERDIR+" -frame "+str(DRQUEUE_FRAME)+" -omultipass "+RENDERDIR+" -threads 0"
+command = ENGINE_PATH + "-nogui -render " + SCENE + " -oimage " + RENDERDIR + " -frame " + str(DRQUEUE_FRAME) + " " + str(BLOCK) + " -omultipass " + RENDERDIR + " -threads " + THREADS
 
 
 print(command)
 sys.stdout.flush()
 
-p = subprocess.Popen(command, shell=True)
+p   = subprocess.Popen(command, shell=True)
 sts = os.waitpid(p.pid, 0)
 
 # display contents of logfile
